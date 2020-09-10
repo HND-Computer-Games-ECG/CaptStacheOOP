15be2c3be46c2ad99d411b6c60018d8746dcd0fb
Nigel Kennington <nigel.kennington@edinburghcollege.ac.uk>
2020-09-10 14:01:40 +0100

    End of class demo.

--- CaptStacheOOP/Game1.cs
+++ CaptStacheOOP/Game1.cs
@@ -9,10 +9,17 @@ namespace CaptStacheOOP
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Static2D background;
+        private Button2D newGame;
+        private Button2D loadGame;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
             IsMouseVisible = true;
         }
 
@@ -27,15 +34,17 @@ namespace CaptStacheOOP
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            background = new Static2D(Content.Load<Texture2D>("GFX\\background"), 0, 0);
+            newGame = new Button2D(Content.Load<Texture2D>("GFX\\MM\\newgame"),
+                                    Content.Load<Texture2D>("GFX\\MM\\mo_newgame"), 0, 0);
+            loadGame = new Button2D(Content.Load<Texture2D>("GFX\\MM\\loadgame"),
+                                    Content.Load<Texture2D>("GFX\\MM\\mo_loadgame"), 0, 100);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            newGame.UpdateMe(Mouse.GetState().Position);
+            loadGame.UpdateMe(Mouse.GetState().Position);
 
             base.Update(gameTime);
         }
@@ -44,7 +53,11 @@ namespace CaptStacheOOP
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            background.DrawMe(_spriteBatch);
+            newGame.DrawMe(_spriteBatch);
+            loadGame.DrawMe(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ CaptStacheOOP/basicGraphics.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptStacheOOP
+{
+    class Static2D
+    {
+        protected Texture2D txr;
+        protected Rectangle bounds;
+
+        // Constructor
+        public Static2D(Texture2D art, int x, int y)
+        {
+            txr = art;
+            bounds = new Rectangle(x, y, art.Width, art.Height);
+        }
+
+        public virtual void DrawMe(SpriteBatch sb)
+        {
+            sb.Draw(txr, bounds, Color.White);
+        }
+    }
+
+    class Button2D : Static2D
+    {
+        private Texture2D txrOn;
+        private bool lit;
+
+        // Constructor
+        public Button2D(Texture2D artPassive, Texture2D artActive, int x, int y) 
+            : base (artPassive, x, y)
+        {
+            txrOn = artActive;
+        }
+
+        public void UpdateMe(Point mousePos)
+        {
+            if (bounds.Contains(mousePos))
+                lit = true;
+            else
+                lit = false;
+        }
+
+        public override void DrawMe(SpriteBatch sb)
+        {
+            if (lit)
+                sb.Draw(txrOn, bounds, Color.White);
+            else
+                sb.Draw(txr, bounds, Color.White);
+        }
+    }
+
+    class AnimatedButton : Button2D
+    {
+        public AnimatedButton (Texture2D artPassive, Texture2D artActive, int x, int y, int frameCount)
+            : base(artPassive, artActive, x, y) 
+        {
+
+        }
+
+    }
+}
